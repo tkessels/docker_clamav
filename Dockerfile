87f7bfa0e3366ffd4be7b32ec42d96b32361f516
schokostreuer <schokostreuer@gmail.com>
2020-01-31 23:47:00 +0100

    added clamav-dev for rar support

--- Dockerfile
+++ Dockerfile
@@ -5,12 +5,13 @@ ARG PGID=1001
 MAINTAINER tabledevil
 
 RUN apk add -u clamav
+RUN apk add -u clamav-dev
 RUN apk add -u freshclam
 RUN freshclam
-
+ADD start.sh /start.sh
+RUN chmod +x /start.sh
 
 RUN addgroup -g ${PGID} user && \
     adduser -D -u ${PUID} -G user user
 
 USER user
-
---
+++ start.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+function start_service() {
+  echo -n "Starting Service... "
+  /etc/init.d/kesl-supervisor start && echo "Done!" || echo "Failed!"
+}
+
+case "${1}" in
+  shell )
+    echo "stage: ${1}"
+    start_service
+    echo "Usage:"
+    cat /root/readme
+    /bin/bash
+    ;;
+  version )
+    echo "stage: ${1}"
+    start_service
+    kesl-control -S --app-info | grep -v '^$'
+    ;;
+  scan )
+    echo "stage: ${1}"
+    start_service
+    echo "Starting Scan of /data:"
+    kesl-control --scan-file --action Skip /data
+    echo "Found Threats"
+    kesl-control -E --query 'EventType == "ThreatDetected"'
+    ;;
+  debug )
+    echo "stage: ${1}"
+    echo "Usage:"
+    cat /root/readme
+    /bin/bash
+    ;;
+esac
